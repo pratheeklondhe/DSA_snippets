efa84c7250b3d1962be0a2683271fd9484c20fe9
Pratheek Londhe <41989079+pratheeklondhe@users.noreply.github.com>
2022-07-04 13:56:50 +0000

    LinkedList size and inserFirst method implemented

---
+++ d19.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+        let count=0, node=this.head;
+        while(node) {
+            count++;
+            node = node.next;
+        }
+        return count;
+  }
+}
+
+let ll = new LinkedList();
+console.log(ll.head);
+ll.insertFirst('HI');
+console.log(ll);
+ll.insertFirst('HI Hello');
+console.log(ll);
